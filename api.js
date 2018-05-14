8a38c45e061540eeeb6994c1577c824b56b1a422
Paweł Kornijów <pawel.kornijow@intive.com>
2018-05-14 10:43:26 +0200

    some simple user api

--- api.js
+++ api.js
@@ -1,36 +1,14 @@
-// hardcoded for now, but sa my comment here states: 
+// hardcoded for now, but as my comment here states: 
 // https://phabricator.automatic.co/T19790
-// this should become a part now newly extracted javascript auth/user api
+// this should become a part of a separate javascript auth/user api
 
 const request = require('superagent');
 
 module.exports = {
   me(access_token) {
-    if (!access_token) {
-      res.sendStatus(401);
-    }
-
-    request
+    return request
       .get('https://api.automatic.com/user/me/')
       .set('Authorization', 'Bearer ' + access_token)
       .set('Accept', 'application/json')
-      .end((err, userRes) => {
-        console.log(userRes)
-          if (err) {
-              if (userRes.unauthorized) {
-                console.log('Unauthorized!');
-                res.sendStatus(401);
-                  // req.session.destroy(function(err) {
-                  //     res.clearCookie('sessionid');
-                  //     res.sendStatus(401);
-                  // });
-              } else {
-                  console.log(err, userRes);
-                  res.sendStatus(500);
-              }
-          } else {
-              res.send(userRes.body);
-          }
-      });
   }
 }
--- routes.js
+++ routes.js
@@ -43,9 +43,23 @@ function accessToken(req, res) {
 //TODO - make token verification part of middleware
 function authorized(req, res) {
   if (req.session.token) {
-    const msg = 'You are logged in. Access Token: ' +  api.me(req.session.token.token.access_token);
-    console.log(msg)
-    res.send(msg);
+    const me = api
+      .me(req.session.token.token.access_token)
+      .then(function (result) {
+        if (result.status in [401, 403]) {
+          console.log('Unauthorized!');
+          req.session.destroy();
+          res.sendStatus(result.status);          
+        } else {
+          const msg = 'You are logged in as ' + result.body.username;
+          console.log(msg);
+          res.send(msg);            
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+      })
   } else {
     res.redirect('/');
   }
